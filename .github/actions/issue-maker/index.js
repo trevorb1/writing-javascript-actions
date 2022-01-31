84892630a3d456b240fb3403d6f1719383f05b24
trevorb1 <trevor_barnes@sfu.ca>
2022-01-30 19:02:15 -0800

    create index.js

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -2,21 +2,22 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 async function run() {
-    try {
-        const issueTitle = core.getInput("issue-title");
-        const jokeBody = core.getInput("joke");
-        const token = core.getInput("repo-token");
-        const octokit = github.getOctokit(token)
+  try {
+    const issueTitle = core.getInput("issue-title");
+    const jokeBody = core.getInput("joke");
+    const token = core.getInput("repo-token");
 
-        const newIssue = await actokit.issues.create({
-            repo: github.context.repo.repo,
-            owner: github.context.repo.owner,
-            title: issueTitle,
-            body: jokeBody
-        });
-    } catch (err) {
-        core.setFailed(err.message);
-    }
+    const octokit = new github.getOctokit(token);
+
+    const newIssue = await octokit.issues.create({
+      repo: github.context.repo.repo,
+      owner: github.context.repo.owner,
+      title: issueTitle,
+      body: jokeBody
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
 run();
